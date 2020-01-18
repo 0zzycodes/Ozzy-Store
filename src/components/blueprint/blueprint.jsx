75bce814fca858f341653732e312e7f041447b20
0zzycodes <horlalekan123@gmail.com>
2020-01-18 11:02:58 +0100

    redesign email recipt template

---
+++ src/components/blueprint/blueprint.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+
+const Blueprint = () => {
+  const strong = {
+    fontSize: '13px',
+    fontWeight: 500
+  };
+  return (
+    <div style={{ color: '#ffffff' }}>
+      <table style={{ marginTop: '100px', width: '100%', textAlign: 'center' }}>
+        <tr>
+          <img src="https://i.ibb.co/j4cR9mN/REMEDII-WHITE.png" alt="Logo" />
+        </tr>
+      </table>
+      <table style={{ padding: '15px' }}>
+        <tr>Dear Ibrahim,</tr>
+        <br />
+        <tr>Thank you for your purchase at Ozzy Store (Order getReference)</tr>
+      </table>
+      <table style={{ padding: '15px' }}>
+        <tr>
+          <span style={{ color: 'orange' }}>Ibrahim </span> <br />
+          <br /> 226 ifedapo owode academy <br /> Ibadan, Nigeria <br /> Phone:
+          09026040774
+        </tr>
+      </table>
+      <table style={{ padding: '15px' }}>
+        <tr style={{ padding: '15px' }}>
+          Thank you. Your order has been received.
+        </tr>
+      </table>
+      <table style={{ padding: '15px' }}>
+        <tr>
+          <td
+            style={{ paddingRight: '30px', fontSize: '13px', fontWeight: 600 }}
+          >
+            DATE: <br /> <br /> <strong style={strong}>03/19/200</strong>
+          </td>
+          <td
+            style={{ paddingRight: '30px', fontSize: '13px', fontWeight: 600 }}
+          >
+            TOTAL: <br /> <br /> <strong style={strong}>₦14340</strong>
+          </td>
+          <td
+            style={{ paddingRight: '30px', fontSize: '13px', fontWeight: 600 }}
+          >
+            {' '}
+            PAYMENT METHOD: <br /> <br />{' '}
+            <strong style={strong}>Direct Transfer</strong>
+          </td>
+        </tr>
+      </table>
+      <table style={{ padding: '15px', textAlign: 'center' }}>
+        <tr style={{ fontSize: '22px' }}>BANK DETAIL</tr>
+      </table>
+      <table style={{ padding: '15px 30px' }}>
+        <tr style={{ fontSize: '18px', color: 'red' }}>REMEDI CLOTHING</tr>
+        <br />
+        <tr>
+          <td
+            style={{ paddingRight: '30px', fontSize: '13px', fontWeight: 600 }}
+          >
+            BANK: <br /> <br />{' '}
+            <strong style={strong}>Guaranty Trust Bank</strong>
+          </td>
+          <td
+            style={{ paddingRight: '30px', fontSize: '13px', fontWeight: 600 }}
+          >
+            ACCOUNT NUMBER: <br /> <br />{' '}
+            <strong style={strong}>12345678</strong>
+          </td>
+        </tr>
+      </table>
+      <table style={{ marginTop: '20px', textAlign: 'center' }}>
+        <tr>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Product
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Desc
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Qty
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Size
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Price
+          </th>
+        </tr>
+        <tr>
+          <td style={{ padding: '15px' }}>
+            <img
+              src="https://storage.googleapis.com/crwn-clothing-e7753.appspot.com/products/k53hueeg5fvth56dou8/imageUrl/img_20200112_174405.jpg"
+              width="65px"
+              height="73.45px"
+              alt=""
+            />
+          </td>
+          <td style={{ padding: '15px', fontSize: '12px' }}>Ship's Cannon</td>
+          <td style={{ padding: '15px' }}>3</td>
+          <td style={{ padding: '15px' }}>L</td>
+          <td style={{ padding: '15px' }}>$100</td>
+        </tr>
+        <tr>
+          <td style={{ padding: '15px' }} colSpan="1">
+            <b>Subtotal:</b>
+          </td>
+          <td>$600</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+export default Blueprint;
--- src/components/payment/structure-message.jsx
+++ src/components/payment/structure-message.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Email, Item, renderEmail } from 'react-html-email';
+import { Email, renderEmail } from 'react-html-email';
+
 
 export const PostFetch = (url, message) => {
   fetch(url, {
@@ -14,44 +15,10 @@ export const PostFetch = (url, message) => {
       console.log(response);
     });
 };
-const makePay = {
-    background: 'black',
-    padding: '20px',
-    bottom: 0,
-    color: 'white',
-    textAlign: 'left',
-    height: '300px'
-  },
-  ul = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alingItems: 'flex-start'
-  },
-  li = {
-    listStyle: 'none',
-    fontSize: '15px',
-    padding: '5px'
-  },
-  strong = {
+const strong = {
     fontSize: '13px',
-    fontWeight: 600
-  },
-  ourBankDetail = {
-    padding: '30px 0px'
-  },
-  h2 = {
-    marginBottom: '20px'
-  },
-  h3 = {
-    fontWeight: 500,
-    color: 'red'
-  },
-  flex = {
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    alingItems: 'center'
-  };
+    fontWeight: 500
+  }
 export const structureOrderMessage = (common, info) => {
   const { intro, name, address, city, country, phone, pacel, total } = common,
     { thank } = info;
@@ -60,81 +27,120 @@ export const structureOrderMessage = (common, info) => {
   return renderEmail(
     <Email title="Your Order">
       <div style={{ padding: '15px' }}>
-        <Item align="left">
-          <p fontSize={12}>{intro},</p>
-          <p fontSize={11}>
-            {thank} <br /> {info.durInfo ? info.durInfo : null}
-          </p>
-        </Item>
-        <h4 style={{ fontSize: '16px' }}>
-          <span style={{ color: 'orange' }}>{name} </span> <br />
+        <table
+        style={{ width: '100%', textAlign: 'center' }}
+      >
+        <tr>
+          <img src="https://i.ibb.co/XZnTp2c/REMEDII.png" alt="Logo" />
+        </tr>
+      </table>
+      <table style={{ padding: '15px' }}>
+        <tr>{intro},</tr>
+        <br/>
+        <tr>{thank} <br /> {info.durInfo ? info.durInfo : null}</tr>
+      </table>
+      <table style={{ padding: '15px' }}>
+        <tr>
+        <span style={{ color: 'orange' }}>{name} </span> <br />
           <br /> {address} <br /> {city}, {country} <br /> Phone: {phone}
-        </h4>
+        </tr>
+      </table>
         {info.durInfo ? (
-          <div style={makePay}>
-            <h6>Thank you. Your order has been received.</h6>
-            <br />
-            <ul style={ul}>
-              <li style={li}>
-                <h6>
-                  DATE:{' '}
-                  <strong style={strong}>{date.toLocaleDateString()}</strong>
-                </h6>
-              </li>
-              <li style={li}>
-                <h6>
-                  TOTAL: <strong style={strong}>₦{total}</strong>
-                </h6>
-              </li>
-              <li style={li}>
-                <h6>
-                  PAYMENT METHOD:{' '}
-                  <strong style={strong}>Direct Transfer</strong>
-                </h6>
-              </li>
-            </ul>
-            <div style={ourBankDetail}>
-              <h2 style={h2}>BANK DETAIL</h2>
-              <h3 style={h3}>REMEDI CLOTHING</h3>
-              <ul>
-                <li style={li}>
-                  <h6>
-                    BANK: <strong style={strong}>Guaranty Trust Bank</strong>
-                  </h6>
-                </li>
-                <li style={li}>
-                  <h6>
-                    ACCOUNT NUMBER: <strong style={strong}>1620207873</strong>
-                  </h6>
-                </li>
-              </ul>
-            </div>
+          <div style={{background: "#000000", color: '#ffffff'}}>
+          <table style={{ padding: '15px' }}>
+        <tr style={{ padding: '15px' }}>
+          Thank you. Your order has been received.
+        </tr>
+      </table>
+      <table style={{ padding: '15px' }}>
+        <tr>
+          <td
+            style={{ paddingRight: '40px', fontSize: '13px', fontWeight: 600 }}
+          >
+            DATE: <br /> <br /> <strong style={strong}>{date.toLocaleDateString()}</strong>
+          </td>
+          <td
+            style={{ paddingRight: '40px', fontSize: '13px', fontWeight: 600 }}
+          >
+            TOTAL: <br /> <br /> <strong style={strong}>₦14340</strong>
+          </td>
+          <td
+            style={{ paddingRight: '40px', fontSize: '13px', fontWeight: 600 }}
+          >
+            {' '}
+            PAYMENT METHOD: <br /> <br />{' '}
+            <strong style={strong}>Direct Transfer</strong>
+          </td>
+        </tr>
+      </table>
+      <table style={{ padding: '15px', textAlign: 'center' }}>
+        <tr style={{ fontSize: '22px' }}>BANK DETAIL</tr>
+      </table>
+      <table style={{ padding: '15px 30px' }}>
+        <tr style={{ fontSize: '18px', color: 'red' }}>REMEDI CLOTHING</tr>
+        <br />
+        <tr>
+          <td
+            style={{ paddingRight: '40px', fontSize: '13px', fontWeight: 600 }}
+          >
+            BANK: <br /> <br />{' '}
+            <strong style={strong}>Guaranty Trust Bank</strong>
+          </td>
+          <td
+            style={{ paddingRight: '40px', fontSize: '13px', fontWeight: 600 }}
+          >
+            ACCOUNT NUMBER: <br /> <br />{' '}
+            <strong style={strong}>12345678</strong>
+          </td>
+        </tr>
+      </table>
           </div>
         ) : null}
-        <h3>Package(s)</h3>
+        <table
+        style={{ marginTop: '30px', textAlign: 'center' }}
+      >
+        <tr style={{ padding: '15px' }}>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Product
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Desc
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Qty
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Size
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Price
+          </th>
+        </tr>
         {pacel.map(eachPacel => {
           const { size, name, quantity, price, imageUrl } = eachPacel;
           return (
-            <div key={`${size}${name}`}>
-              <div style={flex}>
-                <img
-                  src={imageUrl}
-                  width="65px"
-                  height="73.45px"
-                  alt="product"
-                />
-                <span style={{ marginLeft: '20px', paddingBottom: '30px' }}>
-                  <span>Name: {name}</span>
-                </span>
-              </div>
-              <p style={{ marginRight: '5px' }}>
-                Size: {size} <br /> Qty: {quantity} <br /> Price: ₦{price}
-              </p>
-            </div>
-          );
-        })}
-        <br />
-        <h2>Total: ₦{total}</h2>
+        <tr key={`${size}${name}`} style={{ padding: '15px' }}>
+          <td style={{ padding: '15px' }}>
+            <img
+              src={imageUrl}
+              width="65px"
+              height="73.45px"
+              alt="Product"
+            />
+          </td>
+          <td style={{ padding: '15px', fontSize: '12px' }}>{name}</td>
+          <td style={{ padding: '15px' }}>{quantity}</td>
+          <td style={{ padding: '15px' }}>{size}</td>
+          <td style={{ padding: '15px' }}>₦{price}</td>
+        </tr>)})}
+        <tr>
+          <td style={{ padding: '15px' }} colSpan="2">
+            <b>Subtotal:</b>
+          </td>
+          <td>₦{total}</td>
+        </tr>
+      </table>
+        
       </div>
     </Email>
   );
@@ -144,39 +150,67 @@ export const structureMessage = message => {
   return renderEmail(
     <Email title="Your Order">
       <div style={{ padding: '15px' }}>
-        <Item align="left">
-          <p fontSize={12}>New order from Remedi Clothing by {name},</p>
-        </Item>
-        <h4 style={{ fontSize: '16px' }}>
-          <span style={{ color: 'orange' }}>{name} </span> <br />
-          <br /> {address} <br /> {city}, {country} <br /> Phone: {phone} <br />{' '}
-          Email: {email}
-        </h4>
-
-        <h3>Package(s)</h3>
+        <table
+        style={{ width: '100%', textAlign: 'center' }}
+      >
+        <tr>
+          <img src="https://i.ibb.co/XZnTp2c/REMEDII.png" alt="Logo" />
+        </tr>
+      </table>
+      <table style={{ padding: '15px' }}>
+        <tr>New order from Remedi Clothing by {name},</tr>
+      </table>
+      <table style={{ padding: '15px' }}>
+        <tr>
+        <span style={{ color: 'orange' }}>{name} </span> <br />
+          <br /> {address} <br /> {city}, {country} <br /> Phone: {phone} <br/> Email: {email}
+        </tr>
+      </table>
+        <h3>Product Details</h3>
+        <table
+        style={{ marginTop: '30px', textAlign: 'center' }}
+      >
+        <tr style={{ padding: '15px' }}>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Product
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Desc
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Qty
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Size
+          </th>
+          <th style={{ padding: '15px', fontSize: '16px', fontWeight: '400' }}>
+            Price
+          </th>
+        </tr>
         {pacel.map(eachPacel => {
           const { size, name, quantity, price, imageUrl } = eachPacel;
           return (
-            <div key={`${size}${name}`}>
-              <div style={flex}>
-                <img
-                  src={imageUrl}
-                  width="65px"
-                  height="73.45px"
-                  alt="product"
-                />
-                <span style={{ marginLeft: '20px', paddingBottom: '30px' }}>
-                  <span>{name}</span>
-                </span>
-              </div>
-              <p style={{ marginRight: '5px' }}>
-                Size: {size} <br /> Qty: {quantity} <br /> Price: ₦{price}
-              </p>
-            </div>
-          );
-        })}
-        <br />
-        <h2>Total: ₦{total}</h2>
+        <tr style={{ padding: '15px' }}>
+          <td style={{ padding: '15px' }}>
+            <img
+              src={imageUrl}
+              width="65px"
+              height="73.45px"
+              alt="Product"
+            />
+          </td>
+          <td style={{ padding: '15px', fontSize: '12px' }}>{name}</td>
+          <td style={{ padding: '15px' }}>{quantity}</td>
+          <td style={{ padding: '15px' }}>{size}</td>
+          <td style={{ padding: '15px' }}>₦{price}</td>
+        </tr>)})}
+        <tr>
+          <td style={{ padding: '15px' }} colSpan="2">
+            <b>Subtotal:</b>
+          </td>
+          <td>₦{total}</td>
+        </tr>
+      </table>
       </div>
     </Email>
   );
